f71306c6830da5d03e686999987fa676cfff7e88
Andrea Sierra <user@MacBook-Air-de-User.local>
2023-05-31 18:37:46 -0500

    Deposits implementation

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from src.data import ask_deposit, ask_withdraw
-from src.utils import get_printStatement
+from src.data import ask_deposit
+from src.utils import save_movement
 
 
-options = {
-    '1': ('Deposit', ask_deposit()),
-    '2': ('withdraw', ask_withdraw()),
-    '3': ('Statement', get_printStatement)
-}
+options = int(input("Que quieres hacer? \n 1. Deposito \n 2. Retiro \n 3. Ver extracto \n"))
+
+if options == 1:
+    save_movement(ask_deposit())
+
+elif options == 2:
+    print("opcion2")
+
+elif options == 3:
+    print("opcion3")
+
+else:
+    print("No elegiste una opción.")
--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,20 @@
-from src.data import ask_deposit, ask_withdraw
+from datetime import datetime
 
 
-def get_movement():
-  """  #guardar lo movimiento
-    #pizza.append(ingredientes)"""
+def save_movement(deposit):
+    movement = (0, datetime.now(), 0)
+    movements = [movement]
 
-get_movement()
+    # Agregar un movimiento
+    last_movement = movements[-1]
+    balance = last_movement[2] + deposit
+    movement = (deposit, datetime.now(), balance)
+    movements.append(movement)
 
-def get_printStatement():
-    """#imprimir movimiento
+    # Imprimir todos los movimientos
+    for movement in movements:
+        print(f"Monto: {movement[0]}, Fecha: {movement[1]}, Saldo: {movement[2]}")
 
-    #aqui la funcion guardara la lista para el extracto"""
 
-get_printStatement()
+def get_printstatement():
+    print("Hi")
