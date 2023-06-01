a0c07d7fd2e0454e423c4a1466781602ba117c85
Andrea Sierra <user@MacBook-Air-de-User.local>
2023-06-01 14:43:06 -0500

    first version

--- src/data.py
+++ src/data.py
@@ -1,4 +1,4 @@
-def ask_deposit():
+def ask_movement():
     account_deposit = input("Ingresa un monto para depositar: ")
     if account_deposit.isdigit() and int(account_deposit) > 0:
         print(f'El monto es {account_deposit}. ¡El monto que ingresaste es válido!')
@@ -6,13 +6,3 @@ def ask_deposit():
 
     else:
         print("El monto que ingresaste es inválido. Debes ingresar un monto válido")
-
-
-def ask_withdraw():
-    account_withdraw = input("Ingresa un monto para retirar: ")
-    if account_withdraw.isdigit() and int(account_withdraw) > 0:
-        account_withdraw = int(account_withdraw) * -1
-        print(f'El monto a retirar {account_withdraw}. ¡El monto a retirar que ingresaste es válido!')
-        return account_withdraw
-    else:
-        print("El monto a retirar que ingresaste es inválido. Debes ingresar un monto válido")
